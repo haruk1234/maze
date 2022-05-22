81dd69374dc4c2b5c04bd9fe2f213684b891b2f7
haruki1234 <haruki070617@gmail.com>
2022-05-22 19:13:09 +0900

    change a little

--- makemaze.js
+++ makemaze.js
@@ -7,8 +7,8 @@
         while (searchwall(x,y)) {
             for (let ly=0;ly<y;ly++) {
                 for (let lx=0;lx<x;lx++) {
-                    ra = searcharound(lx,ly,x,y)
-                    makenewroad(lx,ly,ra)
+                    ra = searcharound(lx,ly,x,y);
+                    makenewroad(lx,ly,ra);
                 }
             }
         }
@@ -24,23 +24,23 @@
         return false;
     }
     function searcharound(cx,cy,x,y) {
-        ax = cx*2+1
-        ay = cy*2+1
-        ra = []
+        ax = cx*2+1;
+        ay = cy*2+1;
+        ra = [];
         if (maze[ay][ax]==0) {
             return ra;
         }
         if (cy>0&&maze[ay-2][ax]==0) {
-            ra.push(1)
+            ra.push(1);
         }
         if (cx>0&&maze[ay][ax-2]==0) {
-            ra.push(2)
+            ra.push(2);
         }
         if (cy<y-1&&maze[ay+2][ax]==0) {
-            ra.push(3)
+            ra.push(3);
         }
         if (cx<x-1&&maze[ay][ax+2]==0) {
-            ra.push(4)
+            ra.push(4);
         }
         return ra;
     }
@@ -48,35 +48,35 @@
         if (ard.length<1) {
             return false;
         }
-        ax = cx*2+1
-        ay = cy*2+1
+        ax = cx*2+1;
+        ay = cy*2+1;
 
         card = ard[Math.floor(Math.random()*ard.length)] // card - chose around
         if (card==1) {
-            maze[ay-2][ax]=1
-            maze[ay-1][ax]=1
+            maze[ay-2][ax]=1;
+            maze[ay-1][ax]=1;
         }
         if (card==2) {
-            maze[ay][ax-2]=1
-            maze[ay][ax-1]=1
+            maze[ay][ax-2]=1;
+            maze[ay][ax-1]=1;
         }
         if (card==3) {
-            maze[ay+2][ax]=1
-            maze[ay+1][ax]=1
+            maze[ay+2][ax]=1;
+            maze[ay+1][ax]=1;
         }
         if (card==4) {
-            maze[ay][ax+2]=1
-            maze[ay][ax+1]=1
+            maze[ay][ax+2]=1;
+            maze[ay][ax+1]=1;
         }
         return true;
     }
 
     function mazeto3d() {
-        x = maze[0].length
-        y = maze.length
-        scene["maze"] = [[0],[]]
-        fmaze = scene["maze"][1]
-        c = [212,210,254]
+        x = maze[0].length;
+        y = maze.length;
+        scene["maze"] = [[0],[]];
+        fmaze = scene["maze"][1];
+        c = [212,210,254];
         for (let ly=0;ly<y;ly++) {
             for (let lx=0;lx<x;lx++) {
                 if (maze[ly][lx]==0) {
@@ -84,17 +84,26 @@
                     // fmaze.push([[lx,ly,1],[lx+1,ly,1],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],])
                     // fmaze.push([[lx,ly+1,1],[lx,ly,1],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],])
                     // 横面
-                    fmaze.push([[lx,ly,0],[lx,ly,1],[lx,ly+1,0],[0],[c[0]],[c[1]],[c[2]],[0],])
-                    fmaze.push([[lx,ly,1],[lx,ly+1,1],[lx,ly+1,0],[0],[c[0]],[c[1]],[c[2]],[0],])
+                    if (!(lx>0&&maze[ly][lx-1]==0)) {
+                        fmaze.push([[lx,ly,0],[lx,ly,1],[lx,ly+1,0],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                        fmaze.push([[lx,ly,1],[lx,ly+1,1],[lx,ly+1,0],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                    }
                     
-                    fmaze.push([[lx,ly+1,0],[lx,ly+1,1],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],])
-                    fmaze.push([[lx+1,ly+1,0],[lx,ly+1,0],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],])
+                    if (!(ly<y-1&&maze[ly+1][lx]==0)) {
+                        fmaze.push([[lx,ly+1,0],[lx,ly+1,1],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                        fmaze.push([[lx+1,ly+1,0],[lx,ly+1,0],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                    }
                     
-                    fmaze.push([[lx+1,ly+1,0],[lx+1,ly+1,1],[lx+1,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],])
-                    fmaze.push([[lx+1,ly,0],[lx+1,ly+1,0],[lx+1,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],])
+                    if (!(lx<x-1&&maze[ly][lx+1]==0)) {
+                        fmaze.push([[lx+1,ly+1,0],[lx+1,ly+1,1],[lx+1,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                        fmaze.push([[lx+1,ly,0],[lx+1,ly+1,0],[lx+1,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                    }
                     
-                    fmaze.push([[lx+1,ly,0],[lx+1,ly,1],[lx,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],])
-                    fmaze.push([[lx,ly,0],[lx+1,ly,0],[lx,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],])
+                    if (!(ly>0&&maze[ly-1][lx]==0)) {
+                        fmaze.push([[lx+1,ly,0],[lx+1,ly,1],[lx,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                        fmaze.push([[lx,ly,0],[lx+1,ly,0],[lx,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                    }
+
                 }
                 else {
                     fmaze.push([[lx,ly,0],[lx+1,ly,0],[lx+1,ly+1,0],[0],[c[0]],[c[1]],[c[2]],[0],])
