9dbef2c0fe5a95ff8cbc4d7eb8fccaf176f09f48
bem130 <haruki070617@gmail.com>
2022-11-22 22:23:43 +0900

    change a little

--- auto/solve.js
+++ auto/solve.js
@@ -2,14 +2,56 @@ class MazeSolve1 {
     #maze;
     constructor(maze) {
         this.#maze = maze;
-        this.#main();
+        this.main();
+        return this.getResult();
     }
-    #main() {
+    main() {
         let maze = this.#maze;
         console.log(maze);
-        let y = maze.size[0];
-        let x = maze.size[1];
-        let from = new Uint32Array(x*y).fill(0);
-        let head = [];
+        let x = maze.size[0];
+        let y = maze.size[1];
+        this.from = new Uint32Array(x*y).fill(0);
+        this.from[maze.goal[1]*x+maze.goal[0]] = 1;
+        while (this.searcharound()) {
+        }
+    }
+    getResult() {
+        let p = maze.start[1]*maze.size[0]+maze.start[0];
+        let goalp = maze.goal[1]*maze.size[1]+maze.goal[0];
+        let res = [];
+        while (p!=goalp) {
+            res.push(p);
+            p = this.from[p];
+        }
+        res.push(goalp);
+        return res;
+    }
+    searcharound() {
+        let flag = false;
+        let x = maze.size[0];
+        let y = maze.size[1];
+        for (let cy=0;cy<y;cy++) {
+            for (let cx=0;cx<x;cx++) {
+                if (this.#maze.maze[cy][cx]==0) {continue;}
+                if (this.from[(cy)*x+cx]!=0) {continue;}
+                if (this.from[(cy-1)*x+cx]!=0) {
+                    flag = true;
+                    this.from[(cy)*x+cx] = (cy-1)*x+cx
+                }
+                if (this.from[(cy)*x+cx-1]!=0) {
+                    flag = true;
+                    this.from[(cy)*x+cx] = (cy)*x+cx-1
+                }
+                if (this.from[(cy+1)*x+cx]!=0) {
+                    flag = true;
+                    this.from[(cy)*x+cx] = (cy+1)*x+cx
+                }
+                if (this.from[(cy)*x+cx+1]!=0) {
+                    flag = true;
+                    this.from[(cy)*x+cx] = (cy)*x+cx+1
+                }
+            }
+        }
+        return flag;
     }
 }
